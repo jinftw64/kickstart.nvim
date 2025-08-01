06d622a0049f036a8af591f735952a5bb122146c
jinftw64 <103798287+jinftw64@users.noreply.github.com>
2025-08-01 13:35:19 -0400

    add phpactor

--- init.lua
+++ init.lua
@@ -724,6 +724,7 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         pyright = {},
+        phpactor = {},
         rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
